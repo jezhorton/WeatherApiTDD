605d21f0b13f141f1bb2390c9038e83c603d8560
Jez Horton <JHorton@spartaglobal.com>
2019-10-09 09:19:07 +0100

    Initial Commit

---
+++ WeatherAPI.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeatherAPI.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeatherAPI.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c8cbab39-c5eb-4fef-80a5-0ff822bbb3a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WeatherAPI/Tests/TestMain.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace WeatherAPI.Test
+{
+    [TestFixture]
+    public class MainTest
+    {
+        [Test]
+        public void TestWeatherTemp()
+        {
+            
+        }
+        [Test]
+        public void TestWeatherTemp_Min()
+        {
+
+        }
+        [Test]
+        public void TestWeatherTemp_Max()
+        {
+
+        }
+        [Test]
+        public void TestWeatherPressure()
+        {
+
+        }
+        [Test]
+        public void TestWeatherSea_Level()
+        {
+
+        }
+        [Test]
+        public void TestWeatherGrnd_Level()
+        {
+
+        }
+        [Test]
+        public void TestWeatherHumidity()
+        {
+
+        }
+        [Test]
+        public void TestWeatherTemp_Kf()
+        {
+
+        }
+
+    }
+}
